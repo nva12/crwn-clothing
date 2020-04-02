43ce442bc9f8f41d1775e14481c87157b83fc50f
nva12 <nvallee.a7@gmail.com>
2020-04-02 17:43:35 +0400

    add navigation with withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -47,8 +47,15 @@ class Directory extends Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ title, imageUrl, id, size, linkUrl }) => (
+          <MenuItem
+            key={id}
+            title={title}
+            imageUrl={imageUrl}
+            size={size}
+            linkUrl={linkUrl}
+          />
+          /* can also spread other key value pairs: {id, ...otherSectionProps} */
         ))}
       </div>
     );
